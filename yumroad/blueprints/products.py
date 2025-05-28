edafc3859ecb88129e3d1b668ebe49770cb504f9
MASTER\TeoDimas <thdimasmail@gmail.com>
2025-05-28 08:03:36 +0300

    change products layout

--- yumroad/blueprints/products.py
+++ yumroad/blueprints/products.py
@@ -1,23 +1,28 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login import login_required
 
-from yumroad.models import Product
+from yumroad.forms import ProductForm
+from yumroad.models import Product, db
 
-products = Blueprint('products', __name__)
+product_bp = Blueprint('product', __name__)
 
-@products.route('/')
+@product_bp.route('/')
 def index():
     all_products = Product.query.all()
     return render_template('products/index.html', products=all_products)
 
-@products.route('/<product_id>')
+@product_bp.route('/create', methods=['GET', 'POST'])
+@login_required
+def create():
+    form = ProductForm()
+    if form.validate_on_submit():
+        product = Product(name=form.name.data, description=form.description.data)
+        db.session.add(product)
+        db.session.commit()
+        return redirect(url_for('.details', product_id=product.id))
+    return render_template('products/new.html', form=form)
+
+@product_bp.route('/<product_id>')
 def details(product_id):
     product = Product.query.get_or_404(product_id)
-    # Equivalent to:
-    # product = Product.query.get(product_id)
-    # if not product:
-    #    abort(404)
     return render_template('products/details.html', product=product)
-
-@products.errorhandler(404)
-def not_found(exception):
-    return render_template('products/404.html'), 404
